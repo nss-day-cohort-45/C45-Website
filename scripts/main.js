87508661f0e4fd91882de5fa2d21b1044e31d9dd
Rickie Le <rickienthefishies@gmail.com>
2021-04-12 17:22:30 -0500

    styles modals

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 // initialize the tool-tip plugin for Bootstrap4
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="tooltip"]').tooltip()
 })
 
 
@@ -48,7 +48,7 @@ function cohortMembers(list) {
     let studentInfo = `<div class="col-md-3 cohortMems">
           <img class="card-img-top" src="images/classmates/${item.proImg}" alt="${item.firstName} ${item.lastName}" data-toggle="modal" data-target="#cohortMember${item.id}" style="cursor:pointer;">
           <div class="card-body">
-            <h4 class="card-title title-font">${item.firstName} ${item.lastName}</h4>`
+            <h4 class="card-title title-font">${item.firstName} ${item.lastName} 🌹</h4>`
     //if student didn't provide a reelthemin quote then nothing is displayed
     if (item.reelThemIn != null) {
       studentInfo += `<p class="card-text">${item.reelThemIn}</p>`
@@ -56,36 +56,38 @@ function cohortMembers(list) {
     studentInfo += studentContact
 
     //if a student doesn't have a bio, then the learn more button doesn't appear and a modal isn't created
-    if(item.bio != null){
+    if (item.bio != null) {
 
-    studentInfo += `
+      studentInfo += `
             <center><button type="button" class="btn btn-outline-primary title-font bottom" data-toggle="modal" data-target="#cohortMember${item.id}">
-           Learn More!
+           Meet ${item.firstName}
           </button></center>
           </div>
         </div>`
-    //modal info
-    studentInfo +=`
+      //modal info
+      studentInfo += `
         <div class="modal fade" id="cohortMember${item.id}" tabindex="-1" role="dialog" aria-labelledby="cohortMember${item.id}Label" aria-hidden="true">
         <div class="modal-dialog" role="document">
           <div class="modal-content">
             <div class="modal-header">
-           <h5 class="modal-title title-font" id="cohortMember${item.id}Label">${item.firstName} ${item.lastName}</h5>
               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
             <div class="modal-body">
-            <center><img src="images/classmates/${item.funImg}" alt="${item.firstName} ${item.lastName} fun"/></center><br>
-
+              <div class="modal-img-container">
+              <img src="images/classmates/${item.funImg}" class="modal-img" alt="${item.firstName} ${item.lastName} fun"/><br>
+              </div>
+            <h5 class="modal-title title-font" id="cohortMember${item.id}Label">${item.firstName} ${item.lastName}</h5>
             `
 
-    studentInfo += studentContact
+      // studentInfo += studentContact
 
 
-    studentInfo += `
-      
+      studentInfo += `
+
     ${item.bio}
+    <div class="modal-contact">${studentContact}</div>
     </div>
     <center><button type="button" data-dismiss="modal" class="backButton btn btn-outline-primary title-font bottom" aria-label="Close">
       Back
@@ -98,7 +100,7 @@ function cohortMembers(list) {
       studentInfo += `
       </div>
         </div>
-        `
+      `
     }
     document.getElementById("cohort").innerHTML += studentInfo;
 
